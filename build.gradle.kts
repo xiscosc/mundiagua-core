dbb3dde6fdf815087498670eba59823920d4fefe
Francesc Sastre Cabot <francesc.sastrecabot@showpad.com>
2020-11-15 14:59:38 +0100

    remove serverless

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,6 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("com.amazonaws.serverless:aws-serverless-java-container-springboot2:1.5.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/com/xsc/mundiagua/MundiaguaApplication.kt
+++ src/main/kotlin/com/xsc/mundiagua/MundiaguaApplication.kt
@@ -1,16 +1,11 @@
 package com.xsc.mundiagua
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 
 @SpringBootApplication
-class MundiaguaApplication {
-	// silence console logging
-	@Value("\${logging.level.root:OFF}")
-	var message = ""
-}
+class MundiaguaApplication
 
 fun main(args: Array<String>) {
 	runApplication<MundiaguaApplication>(*args)
